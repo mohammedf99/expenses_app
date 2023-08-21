341ba91fb0c14378a4ece54c6948930aa023f6c1
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-21 13:12:10 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'expenses.dart';
+import 'widgets/expenses.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
