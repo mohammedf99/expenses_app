e73e4523dbfd9550964a5e8c51b570f2d27d2ce1
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-24 16:06:20 +0300

    added Dismissible widget to add swipe functionality

--- lib/widgets/expenses_list/expsenses_list.dart
+++ lib/widgets/expenses_list/expsenses_list.dart
@@ -4,15 +4,21 @@ import '../../models/expense.dart';
 import 'expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses, required this.onDismissed});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onDismissed;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onDismissed(expenses[index]),
+        background: const SnackBar(content: Text("Delete")),
+        child: ExpenseItem(expense: expenses[index]),
+      ),
     );
   }
 }
