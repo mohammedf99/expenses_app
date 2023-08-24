2c49a157292c400b3871499e2b9f742bab43e0e9
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-24 16:17:28 +0300

    refactored name

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+import 'expense_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses, required this.onDismissed});
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) onDismissed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onDismissed(expenses[index]),
+        child: ExpenseItem(expense: expenses[index]),
+      ),
+    );
+  }
+}
