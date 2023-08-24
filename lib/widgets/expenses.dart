d9637efd3021ef558caa2ae03ea6c7ddefccc1c6
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-24 16:05:10 +0300

    added add and remove methods as well as a condidition if a list is empty

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,30 +29,51 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (_) {
-        return const NewExpense();
+        return NewExpense(addExpense: _addExpense);
       },
     );
   }
 
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found 🤪"),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      ExpensesList(
+        expenses: _registeredExpenses,
+        onDismissed: _removeExpense,
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expense App'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: _openAddExpenseOverlay,
-          ),
-        ]
-      ),
+      appBar: AppBar(title: const Text('Expense App'), actions: [
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: _openAddExpenseOverlay,
+        ),
+      ]),
       body: Column(
         children: <Widget>[
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: mainContent,
           ),
         ],
       ),
